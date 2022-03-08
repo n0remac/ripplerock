e5dc197e693f78c344b95271e80962825e66d487
Cameron Smart <cameron@rebelliondefense.com>
2022-03-07 22:05:47 -0800

    Add html

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-[label app.py]
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello Sammy!'
+@app.route("/")
+def home():
+    return render_template("RippleRockWebsite.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
