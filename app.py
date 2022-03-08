29cb82ca9ec011c8519c3c2775dfa47616ad4443
Cameron Smart <cameron@rebelliondefense.com>
2022-03-07 23:29:58 -0800

    added projects link

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("RippleRockWebsite.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("Projects/Projects.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
